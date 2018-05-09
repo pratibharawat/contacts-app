4be75df14b470244a4d8acb16381c0f1ddb78fc6
Pratibha Rawat <prawat@amazon.com>
2018-05-08 18:17:10 -0700

    Hooking the app to the backend

--- src/App.js
+++ src/App.js
@@ -1,45 +1,36 @@
-import React, { Component } from 'react';
-import ListContacts from './ListContacts';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 export default class App extends Component {
   state = {
-    contacts: [
-      {
-       id: 'tyler',
-       name: 'Tyler McGinnis',
-       handle: '@tylermcginnis',
-       avatarURL: 'http://localhost:5001/tyler.jpg'
-    },
-     {
-      id: 'karen',
-      name: 'Karen Isgrigg',
-      handle: '@karen_isgrigg',
-      avatarURL: 'http://localhost:5001/karen.jpg'
-    },
-     {
-      id: 'richard',
-      name: 'Richard Kalehoff',
-      handle: '@richardkalehoff',
-      avatarURL: 'http://localhost:5001/richard.jpg'
-    },
-  ],
-  };
-
+    contacts: []
+  }
+  componentDidMount() {
+    ContactsAPI.getAll()
+      .then((contacts) => {
+        this.setState(() => ({
+          contacts
+        }))
+      })
+  }
   removeContact = (contact) => {
     this.setState((currentState) => ({
       contacts: currentState.contacts.filter((c) => {
-          return c.id !== contact.id;
-      }),
-    }));
-  };
+        return c.id !== contact.id
+      })
+    }))
 
+    ContactsAPI.remove(contact)
+  }
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts}
-        onDeleteContact={this.removeContact}
+        <ListContacts
+          contacts={this.state.contacts}
+          onDeleteContact={this.removeContact}
         />
       </div>
-    );
+    )
   }
 }
